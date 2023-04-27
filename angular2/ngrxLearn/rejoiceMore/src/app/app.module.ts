2a6b3e57c6e1beb32721fd6c16ca277b645c1eff
szzmyemail <szzmyemail@126.com>
2023-04-27 13:23:20 +0800

    change name

---
+++ angular2/ngrxLearn/rejoiceMore/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';//, isDevMode
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+//import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    //isDevMode() ? StoreDevtoolsModule.instrument() : []
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
