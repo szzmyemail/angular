a885cf8d8565caa12183517048ef76edefbd0b3c
szzmyemail <szzmyemail@126.com>
2023-08-10 15:45:16 +0800

    lanjiazai

--- angular2/my-app-20230810/src/app/app.component.ts
+++ angular2/my-app-20230810/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angulardemo';
+  title = 'my-app-20230810';
 }
--- angular2/my-app-20230810/src/app/app.module.ts
+++ angular2/my-app-20230810/src/app/app.module.ts
@@ -1,14 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewsComponent } from './components/news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NewsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
