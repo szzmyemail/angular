010de7068d3b4956561d403bdf5af2b7962c3c1a
szzmyemail <szzmyemail@126.com>
2023-08-23 17:30:21 +0800

    trackbt

--- angular2/my-app-20230810/src/app/app.module.ts
+++ angular2/my-app-20230810/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrackByComponent } from './track-by/track-by.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackByComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular2/my-app-20230810/src/app/track-by/track-by.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TrackByComponent } from './track-by.component';
+
+describe('TrackByComponent', () => {
+  let component: TrackByComponent;
+  let fixture: ComponentFixture<TrackByComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TrackByComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TrackByComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
